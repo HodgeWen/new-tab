e981b185cda9fed253d3dc01f52b8a8b776ce5ac
whj <emailwen@qq.com>
2026-01-12 16:02:55 +0800

    fix: 权限相关部分

--- .wxt/types/paths.d.ts
+++ .wxt/types/paths.d.ts
@@ -5,6 +5,7 @@ declare module "wxt/browser" {
   export type PublicPath =
     | ""
     | "/"
+    | "/background.js"
     | "/icons/icon-128.png"
     | "/icons/icon-16.png"
     | "/icons/icon-32.png"
